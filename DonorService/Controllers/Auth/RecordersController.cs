94836cfea4d5ec9c777b2a7dddc29719f7e82ff2
TheCodCat <dimaivaskevic1@gmail.com>
2025-06-05 15:24:39 +0400

    add getallrecords

---
+++ DonorService/Controllers/Auth/RecordersController.cs
@@ -0,0 +1,39 @@
+﻿using Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Models;
+
+namespace DonorService.Controllers.Auth
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RecordersController(DiliveryContext diliveryContext) : ControllerBase
+    {
+        [HttpPost("/Addrecods")]
+        [EndpointSummary("Запись на сдачу")]
+        public async Task<ActionResult> RecordDilivery([FromHeader] int donorid, [FromBody] int diliveryPointId)
+        {
+            var donor = diliveryContext.Donors.FirstOrDefault(x => x.Id == donorid);
+            var point = diliveryContext.DiliveryPoints.FirstOrDefault(x => x.Id == diliveryPointId);
+            Record record = new Record() { DiliveryPoint = point, Donor = donor };
+            diliveryContext.Records.Add(record);
+            await diliveryContext.SaveChangesAsync();
+
+            return Ok("successful recording");
+        }
+
+        [HttpGet("/GetRecords")]
+        [EndpointSummary("Получение всех записей")]
+        public async Task<ActionResult> GetAllRecords()
+        {
+            var records = diliveryContext.Records.
+                Include(x => x.Donor)
+                .Include(x => x.DiliveryPoint)
+                .OrderBy(x => x.Id)
+                .ToList();
+
+            return Ok(records);
+        }
+    }
+}
